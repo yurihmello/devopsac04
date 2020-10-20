e21b7b9ef88d38c844a4d335914c20ccba21e1a5
YURI H MELLO <you@example.com>
2020-10-19 21:22:52 -0300

    ac4

---
+++ devopsac04.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+
+app = Flask(__name__)
+
+@app.route('/')
+def ac04():
+	proximo = 1
+	anterior = 0
+	limite 1000
+	found = 0
+	resposta = "0,"
+	while (found < limite):
+		tmp = proximo
+		proximo = proximo + anterior
+		anterior = tmp
+		found = found + 1
+		resposta += str(proximo) + ","
+
+
+	return resposta
+
+	if __name__ == "__main__":
+		port = int(os.environ.get("PORT", 5000))
+		app.run(host='0.0.0.0', port=port)
